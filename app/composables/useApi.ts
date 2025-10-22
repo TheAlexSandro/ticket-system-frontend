c0a39d186ad40d7b13c53d6d75e1693e125ad357
SCP Foundation ID <jevringarmindo@gmail.com>
2025-10-22 10:31:08 +0700

    updated

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -63,11 +63,11 @@ export function useApi() {
         .then((result: AxiosResponse) => {
           const item = result["data"]["result"];
           var nis = item.nis ? `NIS: ${item.nis}<br>` : "";
-          var kelas = item.kelas ? `Kelas: ${item.kelas}<br>` : "";
+          var kelas = item.kelas ? `${item.kelas}<br>` : "";
           var jantina = item.jantina
-            ? `Jenis Kelamin: ${
+            ? `${
                 item.jantina == "L" ? "Laki-laki" : "Perempuan"
-              }<br>`
+              } - `
             : "";
           var nomor_hp = item.nomor_hp ? `Nomor HP: ${item.nomor_hp}<br>` : "";
           var message = item.is_scanned
